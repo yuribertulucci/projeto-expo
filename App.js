c1ba979905c2b1dc297fc9797f2da527610e85c6
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2025-03-23 19:55:00 -0300

    feat: faz funcionar

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Navigator from "./navigation/Navigator";
+import { useCallback, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import Navigator from './navigation/Navigator'; // Importe o seu navegador
 
-const Tab = createBottomTabNavigator();
+// Evite que a tela de splash seja escondida automaticamente
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-	return (
-		<Navigator />
-	);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // // Carregue fontes ou outros recursos aqui, se necessário
+        // await Font.loadAsync({
+        //   // Exemplo: Carregue fontes personalizadas, se você estiver usando
+        //   'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+        // });
+
+        // Simule um tempo de carregamento (opcional)
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Indique que o app está pronto
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // Esconda a tela de splash quando o app estiver pronto
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null; // Não renderize nada até que o app esteja pronto
+  }
+
+  return (
+    <Navigator onLayout={onLayoutRootView} />
+  );
 }
