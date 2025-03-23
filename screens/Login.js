b7c2d8d35fbe4af38724f633968cc4c522b97b2d
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2025-03-23 20:13:35 -0300

    feat: ao fazer login, adiciona usuario ao AsyncStorage

--- screens/Login.js
+++ screens/Login.js
@@ -1,38 +1,43 @@
-import { View, TextInput, Button, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
+import {View, TextInput, Button, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Login() {
-	const navigation = useNavigation();
-	const [email, setEmail] = useState('');
-	const [senha, setSenha] = useState('');
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
-	const tryLogin = (email, senha) => {
-		if (email === 'admin' && senha === 'admin') {
-			navigation.navigate('MenuPrincipal');
-		} else {
-			alert('Usuário ou senha inválidos');
-		}
-	}
+  const tryLogin = (email, senha) => {
+    if (email === 'admin@admin.com' && senha === 'admin') {
+			const user = {nome: 'Admin', email: 'admin@admin.com', senha: 'admin'};
+      AsyncStorage.setItem('user', JSON.stringify(user))
+        .then(() => {
+          navigation.navigate('MenuPrincipal');
+        });
+    } else {
+      alert('Usuário ou senha inválidos');
+    }
+  }
 
-	return (
-		<View style={{ flex: 1, padding: 20 }}>
-			<Text>Login</Text>
-			<TextInput
-				placeholder="Email"
-				style={{ borderWidth: 1, marginVertical: 10 }}
-				value={email}
-				onChangeText={setEmail}
-			/>
-			<TextInput
-				placeholder="Senha"
-				secureTextEntry
-				style={{ borderWidth: 1, marginVertical: 10 }}
-				value={senha}
-				onChangeText={setSenha}
-			/>
-			<Button title="Entrar" onPress={() => tryLogin(email, senha)} />
-			<Button title="Registrar" onPress={() => navigation.navigate('Registro')} />
-		</View>
-	);
+  return (
+    <View style={{flex: 1, padding: 20}}>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Email"
+        style={{borderWidth: 1, marginVertical: 10}}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Senha"
+        secureTextEntry
+        style={{borderWidth: 1, marginVertical: 10}}
+        value={senha}
+        onChangeText={setSenha}
+      />
+      <Button title="Entrar" onPress={() => tryLogin(email, senha)}/>
+      <Button title="Registrar" onPress={() => navigation.navigate('Registro')}/>
+    </View>
+  );
 }
