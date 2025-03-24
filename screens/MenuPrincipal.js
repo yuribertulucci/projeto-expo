91786073a94d37a3cad03f950af0c758303d5e69
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2025-03-23 21:20:06 -0300

    fix: logout

--- screens/MenuPrincipal.js
+++ screens/MenuPrincipal.js
@@ -1,19 +1,34 @@
 import {View, Button, Text} from 'react-native';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import {useAuth} from "../context/AuthContext";
+import {useEffect, useState} from "react";
 
 export default function MenuPrincipal({ navigation }) {
   const { login, user, loading } = useAuth();
-  console.log("menu:", user);
+  useEffect(() => {
+    if (user === null) {
+      navigation.navigate('Login');
+    }
+  }, [user, navigation]);
 
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Text>Olá, {user.nome}!</Text>
-      <Button title="Lista de Receitas" onPress={() => navigation.navigate('ListaReceitas')} />
-      <Button title="Favoritos" onPress={() => navigation.navigate('Favoritos')} />
-      <Button title="Perfil" onPress={() => navigation.navigate('Perfil')} />
-      <Button title="Pesquisar" onPress={() => navigation.navigate('Pesquisa')} />
-      <Button title="Categorias" onPress={() => navigation.navigate('Categorias')} />
+      <Text style={{ marginBottom: 12, fontSize: 20 }}>Olá, {user?.nome ?? 'erro'}!</Text>
+      <Text>Email: {user?.email ?? 'erro'}</Text>
+      <Text style={{ marginBottom: 24 }}>Senha: {user?.senha ?? 'erro'}</Text>
+      <View style={{
+        flexDirection: 'column',
+        height: 100,
+        justifyContent: 'space-between',
+        marginBottom: 24,
+        marginTop: 24,
+        gap: 12,
+      }}>
+        <Button title="Lista de Receitas" onPress={() => navigation.navigate('ListaReceitas')}/>
+        <Button title="Favoritos" onPress={() => navigation.navigate('Favoritos')}/>
+        <Button title="Perfil" onPress={() => navigation.navigate('Perfil')}/>
+        <Button title="Pesquisar" onPress={() => navigation.navigate('Pesquisa')}/>
+        <Button title="Categorias" onPress={() => navigation.navigate('Categorias')}/>
+      </View>
     </View>
   );
 }
--- screens/Perfil.js
+++ screens/Perfil.js
@@ -1,11 +1,15 @@
 import { View, Text, Button } from 'react-native';
+import {useAuth} from "../context/AuthContext";
 
 export default function Perfil({ navigation }) {
+  const auth = useAuth();
+  const user = auth.user;
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Text>Nome: Usuário</Text>
-      <Text>Email: usuario@email.com</Text>
-      <Button title="Sair" onPress={() => navigation.navigate('Login')} />
+      <Text>Nome: {user?.nome}</Text>
+      <Text>Email: {user?.email}</Text>
+      <Text>Senha: {user?.senha}</Text>
+      <Button title="Sair" onPress={() => auth.logout().then(() => navigation.navigate('Login'))} />
     </View>
   );
 }
